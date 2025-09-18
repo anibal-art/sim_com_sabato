8f3b274cfef8d89f92482941f9e4bcb9ebdf5282
Anibal <anibalvarela134@gmail.com>
2025-09-18 00:26:57 -0300

    guia 1 completa

--- Guia1/simple/simple.f90
+++ Guia1/simple/simple.f90
@@ -1,17 +1,11 @@
 ! Introducción a la Simulación Computacional
 ! Edición: 2025
 ! Docentes: Joaquín Torres y Claudio Pastorino
+! Alumno: Anibal Varela
 
 !Problema 1: El objetivo de este ejercicio es aprender a leer y escribir archivos desde fortran,
 !utilzando las herramientas vistas en clase. Use como punto de partida el paquete simple.tar.gz
 !que se da en la página del curso.
-
-
-
-
-!d) Modifique el programa para que guarde todos los números aleatorios en un vector (array)
-!y sólo escriba al archivo de salida los que cumplen la condición X < Y y también Y > 0,5.
-!Ahora, ¿Qué porcentaje de los números N espera que sean escritos al archivo de salida?
 
 program simple 
 
@@ -23,6 +17,7 @@ program simple
     real (kind=8) :: x(10),a(10,10),b(10,10)
     real (kind=8), allocatable  :: y(:),c(:,:)
     real(kind=8) :: d, e
+    real(kind=8), allocatable :: Xv(:), Yv(:)
 
 ![NO TOCAR] Inicializa generador de número random
 
@@ -72,12 +67,13 @@ program simple
 !b) Utilizando los comandos open, read y close, logre que el programa lea de un archivo externo
 !(’input.dat’) la cantidad de puntos aleatorios N que se van a generar. No olvide crear el
 !archivo ’input.dat’ con su editor preferido1y escribir un número entero en él.
-
+    
     inquire(file='input.dat',exist=es)
     open(unit=10,file='input.dat',status='old')
     read(10,*) n
     close(10)
     print *,"  * La cantidad de numeros leidas de input.dat es: ", n
+
 !b->a) Escriba un programa que genere N = 100 pares de numeros aleatorios (X, Y ) a partir de
 !la función uni() de ziggurat.f90, y sólo escriba a pantalla los que cumplen X < Y .
 
@@ -89,23 +85,30 @@ program simple
 !ción uni() da números reales entre 0 y 1, ¿Qué porcentaje de los números N espera que
 !sean escritos en el archivo de salida?
 
+    allocate(Xv(n), Yv(n))
+
     open(unit=10,file='output.dat',status='unknown')
     
 	do i = 1, n
-		d = uni()
-		e = uni()
-		if (d < e) then
-		    print *, d, e
-            write(10,*) d, e
+        Xv(i) = uni()
+        Yv(i) = uni()
+
+		if (Xv(i) < Yv(i) .and. Yv(i) > 0.5d0) then
+		    print *, Xv(i), Yv(i)
+            write(10,*) Xv(i), Yv(i)
 
 		end if
 	end do
     close(10)
 
+!d) Modifique el programa para que guarde todos los números aleatorios en un vector (array)
+!y sólo escriba al archivo de salida los que cumplen la condición X < Y y también Y > 0,5.
+!Ahora, ¿Qué porcentaje de los números N espera que sean escritos al archivo de salida?
+
 
 
 !! 
-!! F:IN FIN edicion
+!! FIN FIN edicion
 !! 
 ![No TOCAR]
 ! Escribir la última semilla para continuar con la cadena de numeros aleatorios 
